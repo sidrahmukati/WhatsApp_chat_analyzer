9025060772a87f843b0cdcf2619fe7b2eedc5e88
sidrahmukati <sidrahmukati@gmail.com>
2026-09-06 20:14:10 +0500

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.sidebar.title("WhatsApp Chat Analyzer")
 uploaded_file = st.sidebar.file_uploader("Choose a file")
 if uploaded_file is not None:
     bytes_data = uploaded_file.getvalue()
-    data = bytes_data.decode("utf-8")
+    data = bytes_data.decode("utf-8", errors="ignore")
     df = preprocessor.preprocessor(data)
 
     #fetch unique user
